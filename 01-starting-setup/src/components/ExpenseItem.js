a8caca7be099762b3fa5b39f2f540b5fa939e580
isyoo1213 <moethns@gmail.com>
2022-06-29 18:12:45 +0900

    feat: add Dynamic values on JSX

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,24 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+
+    const now = new Date();
+
+    const year = now.getFullYear();
+    const month = now.getMonth()+1;
+    const date = now.getDate();
+    
+    const expenseDate = `${year}.${month}.${date}`
+    
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 30000;
+
   return (
     <div className='expense-item'>
-      <div>Date</div>
+      <div>{expenseDate}</div>
       <div className='expense-item__description'>
-        <h2>Title</h2>
-        <div className='expense-item__price'>Amount</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>{expenseAmount}</div>
       </div>
     </div>
   );
