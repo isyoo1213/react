7e3f20045e4d0dd7867da7dd729cbd16d3b47856
isyoo1213 <moethns@gmail.com>
2022-06-30 02:46:48 +0900

    feat: add Locale Date by adding JS on Component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -2,20 +2,28 @@ import './ExpenseItem.css';
 
 function ExpenseItem(props) {
 
-    const now = new Date();
+    // const now = new Date();
 
-    const year = now.getFullYear();
-    const month = now.getMonth()+1;
-    const date = now.getDate();
+    // const year = now.getFullYear();
+    // const month = now.getMonth()+1;
+    // const date = now.getDate();
+
+    const korYear = props.date.toLocaleString('ko-KR', {year:'numeric'});
+    // const korYear = props.date.getFullYear();
+    const korMonth = props.date.toLocaleString('ko-KR', {month:'long'});
+    const korDay = props.date.toLocaleString('ko-KR', {day:'2-digit'});
     
-    const expenseDate = `${year}.${month}.${date}`
-    
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 30000;
+    // const expenseDate = `${year}.${month}.${date}`
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 30000;
 
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{korYear}</div>  
+        <div>{korMonth}</div>
+        <div>{korDay}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
