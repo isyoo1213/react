eeb50296a935e5c92ae1f17c2a5a76abb4b44fac
isyoo1213 <moethns@gmail.com>
2022-07-02 04:48:28 +0900

    feat: Add Wrapper Component for spliting css

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import ExpenseDate from './ExpenseDate.js';
 import './ExpenseItem.css';
 import './ExpenseDate.js';
+import Card from './Card.js';
 
 function ExpenseItem(props) {
 
@@ -19,13 +20,13 @@ function ExpenseItem(props) {
     // const expenseAmount = 30000;
 
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date}/>  
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
