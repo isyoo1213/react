47fd4177760735341f3c2a67e5f54d1af7b180e4
isyoo1213 <moethns@gmail.com>
2022-07-04 16:17:11 +0900

    feat: add explanations for State

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -27,12 +27,18 @@ const ExpenseItem = (props) => {
     //>>> state 변수가 변할 때 컴포넌트 함수를 다시 호출하고 싶으면 업데이트 함수를 호출하면 됨
     //>>> state 함수가 포함된 컴포넌트를 재평가해야 한다고 알림 >> 컴포넌트 함수와 JSX 재평가
 
+    //>>> Expenses 컴포넌트에서 ExpenseItem 컴포넌트가 4번 호출 됨 >> 서로 독립적으로 관리되는 state
+    //>>> 컴포넌트의 '인스턴스' 기준으로 state를 독립적으로 관리 및 재평가
+
+    //const로 지정한 이유 - 최초의 state상태는 컴포넌트 함수 최초 호출 시에만 props.title을 초기화하고 저장
+    //이후 state변수의 변동은 따로 관리되며 title이라는 변수에 직접 할당이 아닌, 리액트가 관리하는 메모리로부터 받아옴
     const clickHandler = () => {
       setTitle('Updated');
       //업데이트 함수 실행 후 바로 값이 변하는 것이 아닌 해당 state변수의 업데이트를 예약
       //>> 다음 출력에 찍히는 값은 기존의 props.title이 그대로 출력됨
       //>> 이후 컴포넌트 함수, JSX를 재평가 후 업데이트 된 내용을 렌더함
       console.log(title);
+      console.log('ExpenseItem evaluated by React');
     }
 
 
