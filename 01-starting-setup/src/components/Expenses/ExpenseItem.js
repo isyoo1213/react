424d850f0239a99fbb429bd81a745c8e7b66da6f
isyoo1213 <moethns@gmail.com>
2022-07-09 22:51:22 +0900

    feat: Add Expenses Data Dynamically
    - and Comment Updating title button's state by bug

--- 01-starting-setup/src/components/Expenses/ExpenseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItem.js
@@ -21,7 +21,10 @@ const ExpenseItem = (props) => {
     // const expenseAmount = 30000;
 
     // 특별한 변수를 생성하는 것과 유사 >> 컴포넌트 함수가 다시 호출되도록
-    const [title, setTitle] = useState(props.title);
+    
+    //-----섹션5 - 67 State 저장 목록 사용에서 title 업데이트 함수가 문제가 있어 주석처리.------
+    /*const [title, setTitle] = useState(props.title); */
+
     //첫 번째 값 - 변수 자체 / 현재 상태의 값 >> title = props.title
     //두 번쨰 값 - 업데이트 함수 >> 새로운 값을 할당하기 위해 호출할 수 있는 함수
     //>>> state 변수가 변할 때 컴포넌트 함수를 다시 호출하고 싶으면 업데이트 함수를 호출하면 됨
@@ -32,7 +35,9 @@ const ExpenseItem = (props) => {
 
     //const로 지정한 이유 - 최초의 state상태는 컴포넌트 함수 최초 호출 시에만 props.title을 초기화하고 저장
     //이후 state변수의 변동은 따로 관리되며 title이라는 변수에 직접 할당이 아닌, 리액트가 관리하는 메모리로부터 받아옴
-    const clickHandler = () => {
+    
+    //-----섹션5 - 67 State 저장 목록 사용에서 title 업데이트 함수가 문제가 있어 주석처리.------
+    /*const clickHandler = () => {
       setTitle('Updated');
       //업데이트 함수 실행 후 바로 값이 변하는 것이 아닌 해당 state변수의 업데이트를 예약
       //>> 다음 출력에 찍히는 값은 기존의 props.title이 그대로 출력됨
@@ -40,16 +45,18 @@ const ExpenseItem = (props) => {
       console.log(title);
       console.log('ExpenseItem evaluated by React');
     }
+    */
 
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>  
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
+      {/*-----섹션5 - 67 State 저장 목록 사용에서 title 업데이트 함수가 문제가 있어 주석처리.------*/}
+      {/* <button onClick={clickHandler}>Change Title</button> */}
     </Card>
   );
 }
