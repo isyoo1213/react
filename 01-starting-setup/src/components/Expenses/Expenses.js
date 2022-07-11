3843328012d4de1f74f2b2cea5b20dace146475f
isyoo1213 <moethns@gmail.com>
2022-07-12 08:47:41 +0900

    feat: Add Component for printing ExpenseList and Spliting logic

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import ExpenseItem from "./ExpenseItem.js";
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter.js";
+import ExpensesList from './ExpensesList.js';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2022");
@@ -20,20 +20,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContents = <p>No Expenses Found.</p>;
-
-  if(filteredExpenses.length > 0){
-    expensesContents = filteredExpenses.length > 0 &&
-    filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ))
-  }
-
   return (
     <div>
       <Card className="expenses">
@@ -41,7 +27,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContents}
+        <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
   );
