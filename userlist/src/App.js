c01167ce6de341ed8a43ae398f472b1dfd2e55b6
isyoo1213 <moethns@gmail.com>
2022-07-20 10:40:41 +0900

    feat: Add UserList Component and Logic

--- userlist/src/App.js
+++ userlist/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import AddUser from './components/Users/AddUser.js';
+import UsersList from './components/Users/UsersList.js';
 
 
 function App() {
   return (
     <div>
       <AddUser/>
+      <UsersList users={[]}/>
     </div>
   );
 }
---
+++ userlist/src/components/Users/UsersList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styles from "./UsersList.module.css";
+import Card from "../UI/Card.js";
+
+const UsersList = (props) => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li>
+            {user.name} ({user.year}years old)
+          </li>
+        ))}
+        {/*백틱으로 묶어주지 않는 이유는 뭘까*/}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
