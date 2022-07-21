06698de92ea87b98aa016ae37d4e03b7e241ad2a
isyoo1213 <moethns@gmail.com>
2022-07-21 16:04:38 +0900

    feat: Add Portal for Modal and Backdrop

--- userlist/src/components/UI/ErrorModal.js
+++ userlist/src/components/UI/ErrorModal.js
@@ -1,24 +1,48 @@
 import React from "react";
+import ReactDom from "react-dom";
 import Card from "./Card.js";
 import Button from "./Button.js";
 import styles from "./ErrorModal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onErrorConfirm} />;
+};
+
+const MordalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onErrorConfirm}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = (props) => {
   return (
-    <div>
-      <div className={styles.backdrop} onClick={props.onErrorConfirm}/>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={props.onErrorConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(
+        <Backdrop onErrorConfirm={props.onErrorConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {/* 1. createPortal()의 첫 인자로 JSX형식을 사용해야 함 
+          2. 두 번째 인자로 실제 렌더링 되어야 하는 DOM의 컨테이너의 포인터 > DOM API를 활용*/}
+      {/* 실제 해당 컴포넌트가 렌더링되는 AddUser.js에서 설정된 props를 반드시 설정해주어야함 */}
+      {ReactDom.createPortal(
+        <MordalOverlay
+          title={props.title}
+          message={props.message}
+          onErrorConfirm={props.onErrorConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
   );
 };
 
