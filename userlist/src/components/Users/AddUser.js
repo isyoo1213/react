dc8d33cc027d1473cad411b5c141941f8fb2294c
isyoo1213 <moethns@gmail.com>
2022-07-22 13:14:08 +0900

    feat: Change Input-State to Ref

--- userlist/src/components/Users/AddUser.js
+++ userlist/src/components/Users/AddUser.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card.js";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button.js";
@@ -6,13 +6,23 @@ import ErrorModal from "../UI/ErrorModal.js";
 import Wrapper from "../Helpers/Wrapper.js";
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  {
+    /* 생성되는 ref는 '객체' > 항상 current라는 속성을 가지고 있음 > 연결된 ref가 가진 실제 값을 가짐
+      기본값은 undefined로 설정했지만, input에 설정한 ref프랍으로 연결됨
+      실제 연결된 것은 이론적인 값이 아닌 username이라는 className을 가진 실제 DOM 노드 */
+  }
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    console.log(nameInputRef, ageInputRef);
+    console.log(nameInputRef.current.value, ageInputRef.current.value);
+    const enteredName = nameInputRef.current.value;
+    const enteredAge = ageInputRef.current.value;
+    if (enteredName.trim().length === 0 || enteredAge.trim().length === 0) {
       setError({
         title: "Invalid Input",
         message: "Please enter a valid name and age(non-empty values)",
@@ -28,20 +38,10 @@ const AddUser = (props) => {
       console.log("An error occured");
       return;
     }
-    console.log(enteredUsername, enteredAge);
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-    console.log(enteredUsername);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
-    console.log(enteredAge);
+    props.onAddUser(enteredName, enteredAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
+    {/* 원래 DOM요소를 리액트를 거치지 않고 직접적으로 수정하는 것은 바람직하지 않으나, 사용자의 입력값만 바꿔주므로 사용 */}
   };
 
   const errorHandler = () => {
@@ -60,19 +60,11 @@ const AddUser = (props) => {
       <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
-          <input
-            id="username"
-            type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
-          />
+          <input id="username" type="text" ref={nameInputRef} />
+          {/*ref는 어떤 html요소에서도 사용할 수 있는 내장 prop 
+               ref에 담긴 값은 나중에 실제 DOM 요소가 됨*/}
           <label htmlFor="age">Age(Years)</label>
-          <input
-            id="age"
-            type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
-          />
+          <input id="age" type="number" ref={ageInputRef} />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
