b8abac57f529be04d78649b2830794046acc8fd1
isyoo1213 <moethns@gmail.com>
2022-07-23 15:14:06 +0900

    feat: Add useEffect for LocalStorage's login logic

--- useEffect/src/App.js
+++ useEffect/src/App.js
@@ -1,19 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  useEffect(() => {
+    const storedInfo = localStorage.getItem("isLogedIn");
+
+    if (storedInfo === "LOGGED_IN") {
+      setIsLoggedIn(true);
+      {/* useEffect를 사용하지 않을 시, state 설정 함수를 호출할 때마다 컴포넌트를 재평가위해 재실행 > 무한루프 
+          컴포넌트 내에 속해있으므로 모든 컴포넌트의 재평가 후에 리액트에 의해 실행됨*/}
+    }
+  });
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLogedIn", "LOGGED_IN");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
     setIsLoggedIn(false);
   };
 
