74836270df8302d3e4e82842d068b36e71762dd6
isyoo1213 <moethns@gmail.com>
2022-07-25 15:49:06 +0900

    feat: Change Context-Consumer to userContext

--- useEffect/src/components/MainHeader/Navigation.js
+++ useEffect/src/components/MainHeader/Navigation.js
@@ -1,37 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import AuthContext from "../../context/auth-context";
 
 import classes from "./Navigation.module.css";
 
 const Navigation = (props) => {
+  const ctx = useContext(AuthContext);
   return (
-    <AuthContext.Consumer>
-      {/* consumer는 child로 context의 data를 함수형태로 가질 수 있음
-          함수의 return으로 consumer로 감쌌던 JSX코드들을 반환 */}
-      {(ctx) => {
-        return (
-          <nav className={classes.nav}>
-            <ul>
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Users</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <a href="/">Admin</a>
-                </li>
-              )}
-              {ctx.isLoggedIn && (
-                <li>
-                  <button onClick={props.onLogout}>Logout</button>
-                </li>
-              )}
-            </ul>
-          </nav>
-        );
-      }}
-    </AuthContext.Consumer>
+    <nav className={classes.nav}>
+      <ul>
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Users</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <a href="/">Admin</a>
+          </li>
+        )}
+        {ctx.isLoggedIn && (
+          <li>
+            <button onClick={props.onLogout}>Logout</button>
+          </li>
+        )}
+      </ul>
+    </nav>
   );
 };
 
