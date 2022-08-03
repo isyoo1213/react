ba98e1e7a298eeb005306a78548f891d78aac812
isyoo1213 <moethns@gmail.com>
2022-08-03 21:02:16 +0900

    feat: Add useCounter Hook for ForwardCounter component

--- customHook/src/components/ForwardCounter.js
+++ customHook/src/components/ForwardCounter.js
@@ -1,17 +1,21 @@
 import { useState, useEffect } from 'react';
 
 import Card from './Card';
+import useCounter from '../hooks/use-counter';
 
 const ForwardCounter = () => {
-  const [counter, setCounter] = useState(0);
+  const counter = useCounter();
+  {/* 정의해둔 hook을 서로 다른 컴포넌트에서 임포트해서 구현한다해도, 각각의 컴포넌트마다의 상태를 생성함. 공유되는 것 아님 */}
+  
+  // const [counter, setCounter] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
-    }, 1000);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCounter((prevCounter) => prevCounter + 1);
+  //   }, 1000);
 
-    return () => clearInterval(interval);
-  }, []);
+  //   return () => clearInterval(interval);
+  // }, []);
 
   return <Card>{counter}</Card>;
 };
