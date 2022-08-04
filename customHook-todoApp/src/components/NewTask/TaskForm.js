1a7f494e32366880cd0da8532783b33c5bf566e7
isyoo1213 <moethns@gmail.com>
2022-08-04 15:57:15 +0900

    fix: Change variable for import css module

--- customHook-todoApp/src/components/NewTask/TaskForm.js
+++ customHook-todoApp/src/components/NewTask/TaskForm.js
@@ -1,6 +1,6 @@
 import { useRef } from 'react';
 
-import classes from './TaskForm.module.css';
+import styles from './TaskForm.module.css';
 
 const TaskForm = (props) => {
   const taskInputRef = useRef();
@@ -16,7 +16,7 @@ const TaskForm = (props) => {
   };
 
   return (
-    <form className={classes.form} onSubmit={submitHandler}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <input type='text' ref={taskInputRef} />
       <button>{props.loading ? 'Sending...' : 'Add Task'}</button>
     </form>
