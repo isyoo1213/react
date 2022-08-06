844baab2c9ce3fb58c9a3a2c6c7d1abc8d9bcff1
isyoo1213 <moethns@gmail.com>
2022-08-06 22:53:02 +0900

    feat: Add state for reasonable input-state on mounting

--- form-input/src/components/SimpleInput.js
+++ form-input/src/components/SimpleInput.js
@@ -1,9 +1,16 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const nameInputRef = useRef();
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  useEffect(() => {
+    if (enteredNameIsValid) {
+      console.log("Name input is valid!");
+    }
+  }, [enteredNameIsValid]);
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
@@ -12,7 +19,9 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = (event) => {
     event.preventDefault();
 
-    if(enteredName.trim() === ''){
+    setEnteredNameTouched(true);
+
+    if (enteredName.trim() === "") {
       setEnteredNameIsValid(false);
       return;
     }
@@ -20,10 +29,14 @@ const SimpleInput = (props) => {
 
     console.log(enteredName);
     console.log(nameInputRef.current.value);
-    setEnteredName('');
+    setEnteredName("");
   };
 
-  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
+  const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? "form-control invalid"
+    : "form-control";
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -36,7 +49,9 @@ const SimpleInput = (props) => {
           id="name"
           onChange={nameInputChangeHandler}
         />
-        {!enteredNameIsValid && <p className="error-text">Name must not be empty!</p>}
+        {nameInputIsInvalid && (
+          <p className="error-text">Name must not be empty!</p>
+        )}
       </div>
       <div className="form-actions">
         <button>Submit</button>
