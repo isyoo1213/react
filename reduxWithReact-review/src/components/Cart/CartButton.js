25814926fb416baf091afa86fb87cdde7e8f17b9
isyoo1213 <moethns@gmail.com>
2022-08-11 21:39:15 +0900

    feat: Add reducer for showing cart

--- reduxWithReact-review/src/components/Cart/CartButton.js
+++ reduxWithReact-review/src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
 
 const CartButton = (props) => {
+  const disPatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    disPatch(uiActions.toggleCart())  
+  }
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
