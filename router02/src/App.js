24910c0cab6b1884ecfbd2680cbe844dd43779c9
isyoo1213 <moethns@gmail.com>
2022-08-16 00:23:05 +0900

    feat: Add base-routers for project

--- router02/src/App.js
+++ router02/src/App.js
@@ -1,7 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+
+import AllQuotes from './pages/AllQuotes';
+import QuoteDetail from './pages/QuoteDetail';
+import NewQuote from './pages/NewQuote';
+
 function App() {
   return (
     <div>
-
+      <Switch>
+        <Route path='/quotes' exact>
+          <AllQuotes />
+        </Route>
+        <Route path='/quotes/:quoteId' exact>
+          <QuoteDetail />
+        </Route>
+        <Route path='/new-quote' exact>
+          <NewQuote />
+        </Route>
+      </Switch>
     </div>
   );
 }
