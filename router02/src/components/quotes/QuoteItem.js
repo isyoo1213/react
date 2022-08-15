aaed0b068821bbb0be066fce0518b2a27ad0eaff
isyoo1213 <moethns@gmail.com>
2022-08-16 00:08:34 +0900

    feat: Add zip file and settings for practical project

---
+++ router02/src/components/quotes/QuoteItem.js
@@ -0,0 +1,19 @@
+import styles from './QuoteItem.module.css';
+
+const QuoteItem = (props) => {
+  return (
+    <li className={styles.item}>
+      <figure>
+        <blockquote>
+          <p>{props.text}</p>
+        </blockquote>
+        <figcaption>{props.author}</figcaption>
+      </figure>
+      <a className='btn'>
+        View Fullscreen
+      </a>
+    </li>
+  );
+};
+
+export default QuoteItem;
