3f2d95af6fb1afdcf6f3a93da4c37aa7c53d20fc
isyoo1213 <moethns@gmail.com>
2022-08-17 02:54:24 +0900

    feat: Add Dummy Quotes for showing on index page

--- router02/src/pages/AllQuotes.js
+++ router02/src/pages/AllQuotes.js
@@ -1,6 +1,14 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'koala', text: 'Eat, Sleep, Pee & Poo!'},
+  {id: 'q2', author: 'insung', text: 'Eat, Sleep, Code!'},
+  {id: 'q3', author: 'dog', text: 'Eat, Sleep, Bark!'}
+] 
+
 const AllQuotes = () => {
   return (
-    <h1>All Quotes Page</h1>
+    <QuoteList quotes={DUMMY_QUOTES} />
   )
 }
 
