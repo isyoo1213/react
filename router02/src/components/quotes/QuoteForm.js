6d2801f81f049b3f79de6570c2acf6ad30f4df56
isyoo1213 <moethns@gmail.com>
2022-08-17 07:18:34 +0900

    feat: Add Prompt to control prompt according to form state

--- router02/src/components/quotes/QuoteForm.js
+++ router02/src/components/quotes/QuoteForm.js
@@ -1,15 +1,19 @@
-import { useRef } from 'react';
+import { Fragment, useRef, useState } from "react";
+import { Prompt } from "react-router-dom";
 
-import Card from '../UI/Card';
-import LoadingSpinner from '../UI/LoadingSpinner';
-import styles from './QuoteForm.module.css';
+import Card from "../UI/Card";
+import LoadingSpinner from "../UI/LoadingSpinner";
+import styles from "./QuoteForm.module.css";
 
 const QuoteForm = (props) => {
+  const [isEntering, setIsEntering] = useState(false);
   const authorInputRef = useRef();
   const textInputRef = useRef();
 
   function submitFormHandler(event) {
     event.preventDefault();
+
+    console.log(isEntering);
 
     const enteredAuthor = authorInputRef.current.value;
     const enteredText = textInputRef.current.value;
@@ -19,28 +23,47 @@ const QuoteForm = (props) => {
     props.onAddQuote({ author: enteredAuthor, text: enteredText });
   }
 
-  return (
-    <Card>
-      <form className={styles.form} onSubmit={submitFormHandler}>
-        {props.isLoading && (
-          <div className={styles.loading}>
-            <LoadingSpinner />
-          </div>
-        )}
+  const finishEnteringHandler = () => {
+    console.log("finishEnteringHandler triggered")
+    setIsEntering(false);
+    console.log(isEntering);
+  }
 
-        <div className={styles.control}>
-          <label htmlFor='author'>Author</label>
-          <input type='text' id='author' ref={authorInputRef} />
-        </div>
-        <div className={styles.control}>
-          <label htmlFor='text'>Text</label>
-          <textarea id='text' rows='5' ref={textInputRef}></textarea>
-        </div>
-        <div className={styles.actions}>
-          <button className='btn'>Add Quote</button>
-        </div>
-      </form>
-    </Card>
+  const formFocusHandler = () => {
+    console.log("Form has Focused!");
+    setIsEntering(true);
+    console.log(isEntering);
+  };
+
+  return (
+    <Fragment>
+      <Prompt when={isEntering} message={(location)=>'Are you really want to leave the page? All your entered data will be lost!'} />
+      <Card>
+        <form
+          onFocus={formFocusHandler}
+          className={styles.form}
+          onSubmit={submitFormHandler}
+        >
+          {props.isLoading && (
+            <div className={styles.loading}>
+              <LoadingSpinner />
+            </div>
+          )}
+
+          <div className={styles.control}>
+            <label htmlFor="author">Author</label>
+            <input type="text" id="author" ref={authorInputRef} />
+          </div>
+          <div className={styles.control}>
+            <label htmlFor="text">Text</label>
+            <textarea id="text" rows="5" ref={textInputRef}></textarea>
+          </div>
+          <div className={styles.actions}>
+            <button onClick={finishEnteringHandler} className="btn">Add Quote</button>
+          </div>
+        </form>
+      </Card>
+    </Fragment>
   );
 };
 
