89fb0e6298f764810f41d8217215f746dc19c8a9
isyoo1213 <moethns@gmail.com>
2022-08-16 19:45:35 +0900

    feat: Add features for basic setting

--- router02/src/pages/QuoteDetail.js
+++ router02/src/pages/QuoteDetail.js
@@ -1,7 +1,15 @@
+import { Fragment } from "react";
+import { useParams } from "react-router-dom";
+
 const QuoteDetail = () => {
+  const params = useParams();
+
   return (
-    <h1>Quote Detail Page</h1>
-  )
-}
+    <Fragment>
+      <h1>Quote Detail Page</h1>
+      <p>{params.quoteId}</p>
+    </Fragment>
+  );
+};
 
 export default QuoteDetail;
