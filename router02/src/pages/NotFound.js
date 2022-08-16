ecb5b7384f23104109fa0ef4594b29f818f1cb6a
isyoo1213 <moethns@gmail.com>
2022-08-17 04:58:05 +0900

    feat: Add Route for unfound url

---
+++ router02/src/pages/NotFound.js
@@ -0,0 +1,8 @@
+const NotFound = () => {
+  {/* centered는 css모듈이 아닌 index.css에 설정된 전역 클래스 */}
+  return <div className="centered">
+    <p>Page not found</p>
+  </div>
+}
+
+export default NotFound;
