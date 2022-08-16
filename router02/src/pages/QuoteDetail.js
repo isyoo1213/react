cd447c8e9cef607ed073fa03f4a73864ab71d407
isyoo1213 <moethns@gmail.com>
2022-08-17 04:50:17 +0900

    feat: Add Dummy quotes for quoteDetail showing them

--- router02/src/pages/QuoteDetail.js
+++ router02/src/pages/QuoteDetail.js
@@ -2,14 +2,26 @@ import { Fragment } from "react";
 import { useParams, Route } from "react-router-dom";
 
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {id: 'q1', author: 'koala', text: 'Eat, Sleep, Pee & Poo!'},
+  {id: 'q2', author: 'insung', text: 'Eat, Sleep, Code!'},
+  {id: 'q3', author: 'dog', text: 'Eat, Sleep, Bark!'}
+] 
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote)=>{return quote.id === params.quoteId})
+
+  if(!quote){
+    return <p>No Quote Found...</p>
+  }
+
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`} >
         {/* App.js의 Route에 정의된 /quotes/:quoteId <<에 exact 설정 시 url이 1:1대응에서 어긋나므로 풀어주어야함 */}
         <Comments />
