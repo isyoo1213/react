651f46ea8cbc50082f03d2184fd8b6f80e721b84
isyoo1213 <moethns@gmail.com>
2022-08-17 20:55:09 +0900

    feat: Add unCommitted changed

--- customHook/src/hooks/use-counter.js
+++ customHook/src/hooks/use-counter.js
@@ -21,7 +21,7 @@ const useCounter = (forward = true) => {
       clearInterval(interval);
       console.log(`return called`)
     };
-  }, []);{/* effect 작동방법 확인하기 */}
+  }, [forward]);{/* effect 작동방법 확인하기 */}
   {
     /* forward는 useEffect() 내부에서 정의된 것도 아니고, 커스텀 훅의 외부에서 지정된 것도 아니고 매개변수로 받는 값
       >> 의존성으로 추가 >> 의존성이 변경될 때마다 useEffect함수가 재실행  */
