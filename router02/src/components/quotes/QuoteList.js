03636deefc7082093b6173a7b5b538ca3aa8eaa9
isyoo1213 <moethns@gmail.com>
2022-08-18 20:34:00 +0900

    feat: Add useMatchRoute for nested-route

--- router02/src/components/quotes/QuoteList.js
+++ router02/src/components/quotes/QuoteList.js
@@ -1,5 +1,5 @@
 import { Fragment } from 'react';
-import { useHistory, useLocation } from 'react-router-dom';
+import { useHistory, useLocation, useRouteMatch } from 'react-router-dom';
 
 import QuoteItem from './QuoteItem';
 import styles from './QuoteList.module.css';
@@ -20,7 +20,13 @@ const QuoteList = (props) => {
   {/* location 객체에는 최근 로드된 page와 url에 대한 정보
       >> 객체 내 search 속성 개체내에 params에 대한 정보가 담겨있음 */}
 
+  const match = useRouteMatch();
+
+  console.log(match);
   console.log(location);
+  {/* match와 location모두 사용 가능
+      match - path 속성 / location - pathname에 /quotes << 문자정보 확인 가능 */}
+
   const queryParams = new URLSearchParams(location.search);
   {/* QueryParameter와 route 매개변수, :quoteId와 같은 매개변수와의 차이점
       1. 선택적으로 사용가능 - route매칭 자체에 영향을 미치지 않음
@@ -34,8 +40,13 @@ const QuoteList = (props) => {
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
-    history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+    history.push({
+      pathname: location.pathname,
+      search: `?sort=${(isSortingAscending ? 'desc' : 'asc')}`,
+    })
+    // history.push(`${location.pathname}?sort=${(isSortingAscending ? 'desc' : 'asc')}`)
     {/* hisory에서 push를 통해 페이지를 리렌더링 >> 같은 페이지의 모습을 다시 랜더링 하더라도 location 객체의 속성변화 */}
+    {/* location객체를 통해 최대한 하드코딩을 피해 템플릿리터럴로 표현 */}
   }
 
   return (
--- router02/src/pages/QuoteDetail.js
+++ router02/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import { Fragment } from "react";
-import { useParams, Route, Link } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
 import Comments from '../components/comments/Comments';
 import HighlightedQuote from '../components/quotes/HighlightedQuote';
@@ -11,7 +11,12 @@ const DUMMY_QUOTES = [
 ] 
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
+
+  console.log(match);
+  {/* match 객체에는 params외에 path와 url을 구분한 속성이 존재
+      path는 Route를 활용해 정의한 경로, placeholder(:quoteId) */}
 
   const quote = DUMMY_QUOTES.find((quote)=>{return quote.id === params.quoteId})
 
@@ -22,15 +27,21 @@ const QuoteDetail = () => {
   return (
     <Fragment>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path={`/quotes/${params.quoteId}`} exact>
+      <Route path={match.path} exact>
+        {/* Route의 경로정의 !== Link를 통한 concrete value
+            - placeholder가 그대로 존재하는 route 경로로 정의 가능 
+            >>> App.js에서 정의된 root Route의 주소가 변경되더라도, nested Route에서 문자열 주소를 변경해줄 필요 없음*/}
         <div className="centered">
-          <Link to={`/quotes/${params.quoteId}/comments`} >
+          <Link to={`${match.url}/comments`} >
+            {/* Link에 할당되는 concrete value는 match의 url을 통해 특정화해서 가져오기 가능 */}
             Load Comments
           </Link>
         </div>
       </Route>
-      <Route path={`/quotes/${params.quoteId}/comments`} >
-        {/* App.js의 Route에 정의된 /quotes/:quoteId <<에 exact 설정 시 url이 1:1대응에서 어긋나므로 풀어주어야함 */}
+      {/* <Route path={`/quotes/${params.quoteId}/comments`} > */}
+      {/* App.js의 Route에 정의된 /quotes/:quoteId <<에 exact 설정 시 url이 1:1대응에서 어긋나므로 풀어주어야함 */}
+      <Route path={`${match.path}/comments`} >
+
         <Comments />
       </Route>
     </Fragment>
