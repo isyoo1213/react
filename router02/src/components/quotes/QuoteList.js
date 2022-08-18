d7be4ff066f210a62afab9ce8196897b9eb4e691
isyoo1213 <moethns@gmail.com>
2022-08-18 19:49:20 +0900

    feat: Add comments and print log for testing

--- router02/src/components/quotes/QuoteList.js
+++ router02/src/components/quotes/QuoteList.js
@@ -17,16 +17,25 @@ const sortQuotes = (quotes, ascending) => {
 const QuoteList = (props) => {
   const history = useHistory();
   const location = useLocation();
+  {/* location 객체에는 최근 로드된 page와 url에 대한 정보
+      >> 객체 내 search 속성 개체내에 params에 대한 정보가 담겨있음 */}
 
   console.log(location);
   const queryParams = new URLSearchParams(location.search);
+  {/* QueryParameter와 route 매개변수, :quoteId와 같은 매개변수와의 차이점
+      1. 선택적으로 사용가능 - route매칭 자체에 영향을 미치지 않음
+      2. 매칭된 route에 추가적인 behavior - by url을 통한 정보의 저장
+    >> location 객체의 key인 search를 통해 쿼리 파라미터를 객체로 추출 */}
 
   const isSortingAscending = queryParams.get('sort') === 'asc';
+  {/* 객체로 추출된 쿼리파라미터를 key를 통해 value를 추출한 후 비교 */}
+  console.log(isSortingAscending);
 
   const sortedQuotes = sortQuotes(props.quotes, isSortingAscending);
 
   const changeSortingHandler = () => {
     history.push('/quotes?sort=' + (isSortingAscending ? 'desc' : 'asc'))
+    {/* hisory에서 push를 통해 페이지를 리렌더링 >> 같은 페이지의 모습을 다시 랜더링 하더라도 location 객체의 속성변화 */}
   }
 
   return (
