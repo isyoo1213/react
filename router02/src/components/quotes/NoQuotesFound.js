3f665bba8f2b541a5b926d16b8e184b1ac4bd93a
isyoo1213 <moethns@gmail.com>
2022-08-19 19:29:45 +0900

    feat: Add features to bring quotes from firebase

--- router02/src/components/quotes/NoQuotesFound.js
+++ router02/src/components/quotes/NoQuotesFound.js
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 import styles from './NoQuotesFound.module.css';
 
 const NoQuotesFound = () => {
   return (
     <div className={styles.noquotes}>
       <p>No quotes found!</p>
-      <a className='btn'>
+      <Link to='/new-quote' className='btn'>
         Add a Quote
-      </a>
+      </Link>
     </div>
   );
 };
--- router02/src/pages/AllQuotes.js
+++ router02/src/pages/AllQuotes.js
@@ -1,15 +1,39 @@
-import QuoteList from '../components/quotes/QuoteList';
+import { useEffect } from "react";
 
-const DUMMY_QUOTES = [
-  {id: 'q1', author: 'koala', text: 'Eat, Sleep, Pee & Poo!'},
-  {id: 'q2', author: 'insung', text: 'Eat, Sleep, Code!'},
-  {id: 'q3', author: 'dog', text: 'Eat, Sleep, Bark!'}
-] 
+import QuoteList from "../components/quotes/QuoteList";
+import LoadingSpinner from "../components/UI/LoadingSpinner";
+import useHttp from "../hooks/use-http";
+import { getAllQuotes } from "../lib/api";
+import NoQuotesFound from '../components/quotes/NoQuotesFound';
 
 const AllQuotes = () => {
-  return (
-    <QuoteList quotes={DUMMY_QUOTES} />
-  )
-}
+  const {
+    sendRequest,
+    status,
+    data: loadedQuotes,
+    error,
+  } = useHttp(getAllQuotes, true);
+
+  useEffect(()=>{
+    sendRequest();
+  }, [sendRequest]);
+  {/* sendRequest를 종속성에 추가해주는 이유는..? */}
+
+  if(status === 'pending'){
+    return <div className="centered">
+      <LoadingSpinner />
+    </div>
+  }
+
+  if(error){
+    return <p className="centered focused">{error}</p>
+  }
+
+  if(status === 'completed' && (!loadedQuotes || loadedQuotes.length === 0)){
+    return <NoQuotesFound />
+  }
+
+  return <QuoteList quotes={loadedQuotes} />;
+};
 
 export default AllQuotes;
