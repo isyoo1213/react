1e50869b852d51c188fad402ab80878f7ee734c0
isyoo1213 <moethns@gmail.com>
2022-08-24 02:14:58 +0900

    feat: Add features for Another nested-routes

--- routerV502/src/App.js
+++ routerV502/src/App.js
@@ -1,9 +1,9 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate } from "react-router-dom";
 
-import Welcome from './pages/Welcome';
-import Products from './pages/Products';
-import ProductDetail from './pages/ProductDetail';
-import MainHeader from './components/MainHeader';
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import MainHeader from "./components/MainHeader";
 
 function App() {
   return (
@@ -11,15 +11,18 @@ function App() {
       <MainHeader />
       <main>
         <Routes>
-          <Route path='/' exact element={<Navigate replace to='/welcome' />} />
+          <Route path="/" exact element={<Navigate replace to="/welcome" />} />
           {/* element요소로 Navigate JSX를 정의하면 해당 path에 대한 페이지 탐색을 탐색 스택으로 푸시함
               현재 페이지를 새 페이지로 바꾸는 redirect를 원할 경우 replace prop도 추가해야 함
               >> 새 페이지 push !== Redirecting */}
-          <Route path='/welcome/*' element={<Welcome />} />
-          {/* /welcome 만을 통해 welcome의 section내 h1만을 로드하고 싶을 경우
+          <Route path="/welcome/*" element={<Welcome />}>
+            {/* /welcome 만을 통해 welcome의 section내 h1만을 로드하고 싶을 경우
               >> section 내에 정의된 중첩라우트 또한 인식하고 로드할 수 있어야 함 */}
-          <Route path='/products' exact element={<Products />} />
-          <Route path='/products/:productId' element={<ProductDetail />} />
+            <Route path="new-user" element={<p>Welcome, new user!</p>} />
+            {/* 중첩 라우팅이 존재할 경우 이렇게 처리도 가능 */}
+          </Route>
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
       </main>
     </div>
--- routerV502/src/pages/Welcome.js
+++ routerV502/src/pages/Welcome.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Welcome = () => {
   return (
@@ -6,15 +6,13 @@ const Welcome = () => {
       <h1>The Welcome Page</h1>
       <Link to='new-user'>New User</Link>
       {/* 링크 또한 이미 정의된 routes를 통해 로드된 컴포넌트 내에서 정의가 이루어지므로 상대경로 파악함 */}
-      <Routes>
-        <Route path="new-user" element={<p>Welcome, new user!</p>}>
-        </Route>
-      </Routes>
       {/* 변경점
         1. 중첩라우팅 - Routes로 감싸주어야하고 + Route정의 또한 element prop으로 정의
         2. 상위 라우트에서 중첩라우팅을 사용하기 위해 path에 '/*'를 추가해주어야 section으로 묶인 
            <h1>element와 Route인식
         3. 상위 라우트에서 후속적으로 처리되는 것을 인식함으로써 중첩 라우팅의 path에 상위 라우트 주소 삭제 */}
+      <Outlet/>
+      {/* 최상위 라우트에서 중첩 라우트를 정의했을 경우, 해당 내용이 dom의 어느 위치에 표시되어야하는지 마크 */}
     </section>
   );
 };
