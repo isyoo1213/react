488db80e354352bf97b0ceae9bd303e9c6153741
isyoo1213 <moethns@gmail.com>
2022-08-26 01:05:38 +0900

    feat: Add features for Login and getting data

--- authentication/src/components/Auth/AuthForm.js
+++ authentication/src/components/Auth/AuthForm.js
@@ -22,35 +22,50 @@ const AuthForm = () => {
 
     setIsLoading(true);
 
+    let url;
+
     if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBvovDjwcK513u5Wp_QErbdT6wy9LaaXqM";
     } else {
-      fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvovDjwcK513u5Wp_QErbdT6wy9LaaXqM",
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      ).then((res) => {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBvovDjwcK513u5Wp_QErbdT6wy9LaaXqM";
+    }
+
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
         setIsLoading(false);
         if (res.ok) {
+          return res.json();
         } else {
           return res.json().then((data) => {
             //show an error modal
             let errorMessage = "Authentication failed!";
-            if (data && data.error && data.error.message) {
-              errorMessage = data.error.message;
-            }
-            alert(errorMessage);
+            // if (data && data.error && data.error.message) {
+            //   errorMessage = data.error.message;
+            // }
+            throw new Error(errorMessage);
           });
         }
+      })
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        alert(err.message);
       });
+    {
+      /* 오류가 없을 시에는 .then((datda)=>{data})에서 if분기에 따른 return데이터를 받고, 오류가 있을 시 응답코드를 변형해 errorMessage를 오류에 담아 catch 실행 */
     }
   };
 
@@ -72,8 +87,10 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          { !isLoading && <button>{isLogin ? "Login" : "Create Account"}</button> }
-          { isLoading && <p>Sending request...</p>}
+          {!isLoading && (
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+          )}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
