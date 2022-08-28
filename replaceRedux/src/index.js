225b7ef0e1eac609be2d1909b89d312b144a74a0
isyoo1213 <moethns@gmail.com>
2022-08-28 13:17:03 +0900

    feat: Add useContext for showing Products

--- replaceRedux/src/index.js
+++ replaceRedux/src/index.js
@@ -1,25 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { combineReducers, createStore } from "redux";
 import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import productReducer from "./store/reducers/products";
-
-const rootReducer = combineReducers({
-  shop: productReducer,
-});
-
-const store = createStore(rootReducer);
+import { ProductsContextProvider } from "./context/products-context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <Provider store={store}>
+  <ProductsContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
+  </ProductsContextProvider>
 );
