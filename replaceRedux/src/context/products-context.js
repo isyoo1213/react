7b653938a9379192adff09569ef164a3e73fac04
isyoo1213 <moethns@gmail.com>
2022-08-29 11:35:17 +0900

    feat: Add features for Favorite toggling and showing

--- replaceRedux/src/context/products-context.js
+++ replaceRedux/src/context/products-context.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 const ProductsContext = React.createContext({
   products: [],
+  toggleFav: (id)=>{}
 });
 
 export const ProductsContextProvider = (props) => {
@@ -30,9 +31,29 @@ export const ProductsContextProvider = (props) => {
       description: "Street style! An orange hat.",
       isFavorite: false,
     },
-  ]);
+  ])
+
+  const toggleFavorite = (productId) => {
+    setProductsList((currentProductList)=>{
+      const productIndex = currentProductList.findIndex(
+        product => product.id === productId
+      );
+      const newFavStatus = !currentProductList[productIndex].isFavorite;
+      const updatedProducts = [...currentProductList];
+      updatedProducts[productIndex] = {
+        ...currentProductList[productIndex],
+        isFavorite: newFavStatus
+      }
+      //하드코딩을 피하는 이유는..? + 구조분해할당시 ... 문법을 통해 깊은복사 + 새로운 값 할당하는 부분 확인
+  
+  
+      return updatedProducts;
+    })
+    //가장 최근의 state로 작업하기 위해 함수형으로 작성
+  }
+
   return (
-    <ProductsContext.Provider value={{ products: productsList }}>
+    <ProductsContext.Provider value={{ products: productsList, toggleFav:toggleFavorite }}>
       {props.children}
     </ProductsContext.Provider>
   );
